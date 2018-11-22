b610c05e16e92d79fbb83bc0b0c27cc8d96068a0
Valto Silva <valtosilva7@gmail.com>
2018-11-22 13:05:52 -0500

    Added Start/Finish components/Routes | Updated App module/component

--- projects/src/app/app.module.ts
+++ projects/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { WorkoutRunnerModule } from './workout-runner/workout-runner.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { WorkoutRunnerModule } from './workout-runner/workout-runner.module';
+import { StartComponent } from './start/start.component';
+import { FinishComponent } from './finish/finish.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    FinishComponent
   ],
   imports: [
     BrowserModule,
-    WorkoutRunnerModule
+    WorkoutRunnerModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
