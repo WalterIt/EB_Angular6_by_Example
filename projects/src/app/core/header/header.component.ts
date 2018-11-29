ed9ecc97d0a61b0c15b48b9e7be021e167cc88e6
Valto Silva <valtosilva7@gmail.com>
2018-11-29 16:07:52 -0500

    Added ngx-modialog package | Implemented header and video-player components

--- projects/src/app/core/header/header.component.ts
+++ projects/src/app/core/header/header.component.ts
@@ -12,6 +12,7 @@ import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   private showHistoryLink = true;
+
   constructor(private router: Router) {
     this.router.events.pipe(
       filter(e => e instanceof NavigationEnd))
