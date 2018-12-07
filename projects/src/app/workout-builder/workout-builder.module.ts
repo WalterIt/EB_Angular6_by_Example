29a003aea4e7edea6ccc2f6b471edccfef2f05af
Valto Silva <valtosilva7@gmail.com>
2018-12-06 19:12:24 -0500

    Reactive Forms: Using FormBuilder API | Validations | Custom Validators:alphanumeric-validator | Updated: Exercises components & workout-builder.module.

--- projects/src/app/workout-builder/workout-builder.module.ts
+++ projects/src/app/workout-builder/workout-builder.module.ts
@@ -5,6 +5,14 @@ import { NgModule } from '@angular/core';
    time, which would generate an error when we get to implementing lazy loading for this module.
  */
 import { CommonModule } from '@angular/common';
+/**
+ * Reactive forms use three new form directives that we haven't discussed before: FormGroup, FormControl, and
+ *  FormArray. These directives allow the form object that is constructed in code to be tied directly to the HTML
+ *  markup in the template. The form controls that are created in the component class are then directly available
+ *  in the form itself. Technically speaking, this means that we don't need to use ngModel (which is integral to
+ *  template-driven forms) with reactive forms (although it can be used). The overall approach is a cleaner and less
+ *  cluttered template with more focus on the code that drives the form. Let's get started with building a reactive form.
+ */
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { WorkoutBuilderRoutingModule } from './workout-builder-routing.module';
